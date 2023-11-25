8f60afb48f189f3172eb4e1e8d6af02c74a142ea
RichChien123 <128145124+RichChien123@users.noreply.github.com>
2023-11-25 21:22:18 +0800

    Update scripts.js

--- baybay/scripts.js
+++ baybay/scripts.js
@@ -1,43 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('change-language-button');
-    let currentLanguage = 'english'; // Default language
-    const chineseURL = 'https://github.com/RichChien123/cleaning-beach/raw/ff65c2fe7f7db6d4a1a95bfaf9bbf956c9f9580c/baybay/languages/chinese.json';
-    const englishURL = 'https://github.com/RichChien123/cleaning-beach/raw/ff65c2fe7f7db6d4a1a95bfaf9bbf956c9f9580c/baybay/languages/english.json';
+document.addEventListener("DOMContentLoaded", function() {
 
-    // Function to load and apply translations
-    function loadTranslations(language) {
-        const languageURL = language === 'english' ? englishURL : chineseURL;
-        fetch(languageURL)
-            .then(response => response.json())
-            .then(data => applyTranslations(data))
-            .catch(error => {
-                console.error('Error loading language file:', error);
-            });
-    }
-
-    // Function to apply translations to elements with data-translate attribute
-    function applyTranslations(data) {
-        const elementsToTranslate = document.querySelectorAll('[data-translate]');
-        elementsToTranslate.forEach(element => {
-            const translationKey = element.getAttribute('data-translate');
-            if (data.hasOwnProperty(translationKey)) {
-                element.textContent = data[translationKey];
+    // Example of a simple scroll animation for sections
+    const sections = document.querySelectorAll('section');
+    window.addEventListener('scroll', () => {
+        const scrollPosition = window.scrollY + window.innerHeight;
+        sections.forEach(section => {
+            if (section.offsetTop + section.offsetHeight / 3 < scrollPosition) {
+                section.style.opacity = 1;
+                section.style.transform = 'translateY(0)';
             }
         });
-    }
-
-    // Toggle language when the button is clicked
-    toggleButton.addEventListener('click', function () {
-        if (currentLanguage === 'english') {
-            currentLanguage = 'chinese';
-        } else {
-            currentLanguage = 'english';
-        }
-
-        // Load and apply translations for the selected language
-        loadTranslations(currentLanguage);
     });
 
-    // Initial translation
-    loadTranslations(currentLanguage);
+    // Add more interactive or animation scripts here
+
 });
