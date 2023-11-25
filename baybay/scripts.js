6c272574d97a686b262b27301bec92f56cfd1e6e
RichChien123 <128145124+RichChien123@users.noreply.github.com>
2023-11-26 00:09:41 +0800

    Update scripts.js

--- baybay/scripts.js
+++ baybay/scripts.js
@@ -1,17 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
 
-    // Example of a simple scroll animation for sections
-    const sections = document.querySelectorAll('section');
-    window.addEventListener('scroll', () => {
-        const scrollPosition = window.scrollY + window.innerHeight;
-        sections.forEach(section => {
-            if (section.offsetTop + section.offsetHeight / 3 < scrollPosition) {
-                section.style.opacity = 1;
-                section.style.transform = 'translateY(0)';
+    // Example of smooth scroll for navigation links
+    const navLinks = document.querySelectorAll('nav a[href^="#"]');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            let target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop,
+                    behavior: 'smooth'
+                });
             }
         });
     });
 
-    // Add more interactive or animation scripts here
+    // Example of simple form validation
+    const contactForm = document.querySelector('#contact form');
+    contactForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        // Add form validation logic here
+        let name = this.querySelector('input[name="name"]').value;
+        let email = this.querySelector('input[name="email"]').value;
+        let message = this.querySelector('textarea[name="message"]').value;
+
+        if (name && email && message) {
+            // Process form data here
+            console.log('Form submitted:', { name, email, message });
+            // You can also add an AJAX request to send the data to a server
+        } else {
+            alert('Please fill out all fields.');
+        }
+    });
+
+    // Add more interactive or animation scripts as needed
 
 });
